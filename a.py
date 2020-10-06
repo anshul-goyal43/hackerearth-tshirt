9502629aed7e0b07ef294bb9965af06b64b9f4d5
Anshul Goyal <anshul.goyal@research.iiit.ac.in>
2020-10-06 21:54:04 +0530

    Update a.py

--- a.py
+++ a.py
@@ -1,21 +1,19 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
-from selenium.webdriver import ActionChains
 from time import sleep
 
 driver = webdriver.Firefox()
 driver.get("https://tezconnect.typeform.com/to/cXgWp2")
-sleep(9)
+sleep(3)
 s= driver.find_element_by_class_name('button__ButtonWrapper-sc-1g3rldj-0')
 s.click()
-sleep(9)
-#name = driver.find_elements_by_class_name('spacer__Spacer-sc-11bdvt0-0')
-#name[0].click()
+sleep(3)
 na = driver.find_elements_by_class_name('input__InputField-p8yw0p-0')
-na[0].send_keys("some text",Keys.ENTER)
-sleep(9)
-#aa = driver.find_elements_by_class_name('input__InputField-p8yw0p-0')
-na[1].send_keys("some text",Keys.ENTER)
-sleep(8)
+na[0].send_keys("some tsjjext",Keys.ENTER)
+sleep(2)
+na[1].send_keys("some tegggsxt",Keys.ENTER)
+sleep(3)
 x = driver.find_element_by_xpath("//input[@placeholder='name@example.com']")
-x.send_keys("some@gmail.com",Keys.ENTER)
+#print(x)
+x.send_keys("shhhhasmes@gmail.com")
+x.send_keys(Keys.CONTROL,Keys.ENTER)
